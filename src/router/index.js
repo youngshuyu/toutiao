6ec41d586973c480da5701977cd7883f0641daae
ysy <youngshuyu@gmail.com>
2019-12-18 17:11:03 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // 按需加载 => 只有需要访问的时候 才进行js的请求,否则不请求
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
