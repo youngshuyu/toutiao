6437de5b8551689a46de653d0de2635d37262292
ysy <youngshuyu@gmail.com>
2019-12-22 18:01:53 +0800

    评论列表-页面结构

--- src/views/home/comment/comment.vue
+++ src/views/home/comment/comment.vue
@@ -1,5 +1,16 @@
 <template>
-  <p>这是评论</p>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">评论列表</template>
+    </bread-crumb>
+    <el-table>
+      <el-table-column label="标题" width="600"></el-table-column>
+      <el-table-column label="评论状态"></el-table-column>
+      <el-table-column label="总评论数"></el-table-column>
+      <el-table-column label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作"></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
