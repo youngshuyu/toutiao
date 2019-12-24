077dd17f0bdeed093af97d64643d4cd7db46d1d9
ysy <youngshuyu@gmail.com>
2019-12-24 16:13:22 +0800

    列表分页及请求

--- src/views/home/comment/comment.vue
+++ src/views/home/comment/comment.vue
@@ -11,10 +11,23 @@
       <el-table-column label="操作">
         <template slot-scope="obj">
           <el-button type="text" size="small">修改</el-button>
-          <el-button type="text" size="small" @click="openOrClose(obj.row)">{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
+          <el-button
+            type="text"
+            size="small"
+            @click="openOrClose(obj.row)"
+          >{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页组件 -->
+    <el-row style="height:80px" align="middle" type="flex" justify="center">
+      <el-pagination background layout="prev, pager, next"
+       :total="page.total"
+       :current-page="page.currentPage"
+       :page-sizes="page.pageSize"
+       @current-change="changePage">
+      </el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -22,18 +35,27 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        total: 100,
+        currentPage: 1,
+        pageSize: 10
+      }
     }
   },
   methods: {
     getData () {
       this.$axios({
         url: 'articles',
-        params: { response_type: 'comment' }
+        params: { response_type: 'comment', page: this.page.currentPage, per_page: this.page.pageSize }
+      }).then(res => {
+        this.list = res.data.results
+        this.page.total = res.data.total_count
       })
-        .then(res => {
-          this.list = res.data.results
-        })
+    },
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getData()
     },
     formatterBool (row, column, cellValue, index) {
       // row 当前行数据
@@ -44,27 +66,25 @@ export default {
     },
     openOrClose (row) {
       let mess = row.comment_status ? '关闭' : '打开'
-      this.$confirm(`你确定要${mess}评论吗`)
-        .then(() => {
-          this.$axios({
-            method: 'put',
-            url: '/comments/status',
-            params: {
-              article_id: row.id.toString()
-            },
-            data: {
-              allow_comment: !row.comment_status
-            }
+      this.$confirm(`你确定要${mess}评论吗`).then(() => {
+        this.$axios({
+          method: 'put',
+          url: '/comments/status',
+          params: {
+            article_id: row.id.toString()
+          },
+          data: {
+            allow_comment: !row.comment_status
+          }
+        }).then(res => {
+          // 打开或者关闭评论成功之后
+          this.$message({
+            type: 'success',
+            message: '操作成功'
           })
-            .then(res => {
-              // 打开或者关闭评论成功之后
-              this.$message({
-                type: 'success',
-                message: '操作成功'
-              })
-              this.getData()// 重新请求列表
-            })
+          this.getData() // 重新请求列表
         })
+      })
     }
   },
   created () {
@@ -74,5 +94,4 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
 </style>
