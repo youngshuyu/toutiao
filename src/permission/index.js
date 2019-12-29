eea097773445e80ee3e99256af50e9bd4b06146c
ysy <youngshuyu@gmail.com>
2019-12-29 10:26:10 +0800

    引入进度条

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,7 +1,11 @@
 // 权限拦截 守卫导航
 import router from '../router'
+// 引入进度条组件
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 全局前置守卫
 router.beforeEach(function (to, from, next) {
+  progress.start()
   // 拦截谁 判断拦截地址
   if (to.path.startsWith('/home')) {
     // 进行权限判断，判断有无token
@@ -14,4 +18,7 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()// 直接放行
   }
+})
+router.afterEach(function () {
+  progress.done()
 })
