f0dbb8a4452ee67950a2c11ca367a4db668b72f6
ysy <youngshuyu@gmail.com>
2019-12-29 10:14:21 +0800

    应用echarts

--- src/views/home/home/home.vue
+++ src/views/home/home/home.vue
@@ -1,17 +1,158 @@
 <template>
-  <div class="bg"></div>
+  <div>
+    <div class="chart1" ref="myChart"></div>
+    <div class="chart2" ref="myChart2"></div>
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
+  mounted () {
+    let myChart = echarts.init(this.$refs.myChart)
+    var base = +new Date(1968, 9, 3)
+    var oneDay = 24 * 3600 * 1000
+    var date = []
 
+    var data = [Math.random() * 300]
+
+    for (var i = 1; i < 20000; i++) {
+      var now = new Date((base += oneDay))
+      date.push(
+        [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/')
+      )
+      data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]))
+    }
+    let option = {
+      tooltip: {
+        trigger: 'axis',
+        position: function (pt) {
+          return [pt[0], '10%']
+        }
+      },
+      title: {
+        left: 'center',
+        text: '大数据量面积图'
+      },
+      toolbox: {
+        feature: {
+          dataZoom: {
+            yAxisIndex: 'none'
+          },
+          restore: {},
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: date
+      },
+      yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%']
+      },
+      dataZoom: [
+        {
+          type: 'inside',
+          start: 0,
+          end: 10
+        },
+        {
+          start: 0,
+          end: 10,
+          handleIcon:
+            'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+          handleSize: '80%',
+          handleStyle: {
+            color: '#fff',
+            shadowBlur: 3,
+            shadowColor: 'rgba(0, 0, 0, 0.6)',
+            shadowOffsetX: 2,
+            shadowOffsetY: 2
+          }
+        }
+      ],
+      series: [
+        {
+          name: '模拟数据',
+          type: 'line',
+          smooth: true,
+          symbol: 'none',
+          sampling: 'average',
+          itemStyle: {
+            color: 'rgb(255, 70, 131)'
+          },
+          areaStyle: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: 'rgb(255, 158, 68)'
+              },
+              {
+                offset: 1,
+                color: 'rgb(255, 70, 131)'
+              }
+            ])
+          },
+          data: data
+        }
+      ]
+    }
+    myChart.setOption(option)
+
+    let myChart2 = echarts.init(this.$refs.myChart2)
+    let option2 = {
+      title: {
+        text: '某站点用户访问来源',
+        subtext: '纯属虚构',
+        x: 'center'
+      },
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+      },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '60%'],
+          data: [
+            { value: 335, name: '直接访问' },
+            { value: 310, name: '邮件营销' },
+            { value: 234, name: '联盟广告' },
+            { value: 135, name: '视频广告' },
+            { value: 1548, name: '搜索引擎' }
+          ],
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    }
+    myChart2.setOption(option2)
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.bg {
-    background-image: url('../../../assets/img/老婆.jpeg');
-    height: 100vh;
-    background-size: cover;
+.chart1 {
+  width: 1100px;
+  height: 500px;
+}
+.chart2  {
+  margin: 50px auto;
+  width: 600px;
+  height: 600px;
 }
 </style>
